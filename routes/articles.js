c731e5a388391b6aa00787f514e13484c2708b14
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-29 17:47:34 +0100

    add POST route and return new article on success

--- routes/articles.js
+++ routes/articles.js
@@ -2,4 +2,19 @@ const express = require('express');
 const router = express.Router();
 const uniqid = require('uniqid');
 
+router.post('/', (req, res) => {
+    try {
+        const article = {
+            //for tests only when id is passed into test POST data req.body.id used instead of uniqid
+            id: req.body.id || uniqid(),
+            article: req.body.article
+        }
+        req.app.db.get("drafts").push(article).write()
+        res.json({ article })
+    } catch (error) {
+        console.error(error)
+        res.status(500).json({ error: 'Error' })
+    }
+})
+
 module.exports = router;
