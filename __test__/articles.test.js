b14f0a678476f64455affee3410ff4839da00b50
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-29 16:49:39 +0100

    add todos for required tests

--- __test__/articles.test.js
+++ __test__/articles.test.js
@@ -1,5 +1,23 @@
 const request = require("supertest");
 const app = require('../app');
 
+describe('When user makes a POST request', () => {
+    it.todo("POST '/' should respond status code 200 and new Article")
+    it.todo("empty POST '/' should respond status code 400 and error message")
+})
 
+describe('When user makes a GET request', () => {
+    it.todo("GET '/' should respond status code 200")
+    it.todo("GET '/:id' should respond status code 200")
+})
+
+describe('When user makes a PUT request', () => {
+    it.todo("PUT '/:id' should respond status code 200 and updated Article")
+    it.todo("empty PUT '/:id' should respond status code 400 and error message")
+})
+
+describe('When user makes a DELETE request', () => {
+    it.todo("DELETE '/:id' should respond status code 200 and updated Article")
+    it.todo("non-existent DELETE '/:id' should respond status code 404 and error message")
+})
 
