c942b6f01367df4355779f7f837c17dc1f53cffd
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-30 19:44:02 +0100

    user express.json to parse body

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const low = require("lowdb");
 const cors = require('cors')
 const FileSync = require('lowdb/adapters/FileSync');
 const articles = require("./routes/articles");
+var bodyParser = require('body-parser')
 
 const db = low(new FileSync('db.json'));
 
@@ -12,6 +13,7 @@ db.defaults({
 
 const app = express();
 app.db = db;
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cors())
 app.use("/", articles);
