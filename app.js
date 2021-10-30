1a7fefa578dabacce5887bc488dbf8e8f03cb5c7
Yasin Amin <yasin.amin@hotmail.co.uk>
2021-10-30 15:08:31 +0100

    add cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const low = require("lowdb");
+const cors = require('cors')
 const FileSync = require('lowdb/adapters/FileSync');
 const articles = require("./routes/articles");
 
@@ -12,6 +13,7 @@ db.defaults({
 const app = express();
 app.db = db;
 app.use(express.urlencoded({ extended: false }));
+app.use(cors())
 app.use("/", articles);
 
 module.exports = app;
